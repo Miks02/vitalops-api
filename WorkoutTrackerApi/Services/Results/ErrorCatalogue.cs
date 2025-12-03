838e71fff899bd39b2024a755d5ac47bd993729b
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-03 09:35:56 +0100

    feat(result_pattern): Implement a ServiceResult class that acts as a modern result pattern for handling expected errors and add an error catalogue with static error classes

---
+++ WorkoutTrackerApi/Services/Results/ErrorCatalogue.cs
@@ -0,0 +1,93 @@
+namespace WorkoutTrackerApi.Services.Results;
+
+public sealed class Error
+{
+    public string Code { get; }
+    public string Description { get; }
+
+    private Error(string code, string description)
+    {
+        if (string.IsNullOrWhiteSpace(code))
+            throw new ArgumentException("Code cannot be null or whitespace", nameof(code));
+
+        if (string.IsNullOrWhiteSpace(description))
+            throw new ArgumentException("Description cannot be null or whitespace", nameof(description));
+
+        Code = code;
+        Description = description;
+    }
+
+    public static class General
+    {
+        
+        public static Error NotFound(string message = "The requested resource was not found")
+            => new("General.NotFound", message);
+
+        public static Error InternalServerError(string message = "Internal server error")
+            => new("General.InternalServerError", message);
+
+        public static Error UnknownError(string message = "An unknown error occurred")
+            => new("General.UnknownError", message);
+    }
+    
+    public static class Resource
+    {
+        public static Error NotFound(string resourceName, string identifier = "")
+        {
+            string message = string.IsNullOrEmpty(identifier)
+                ? $"{resourceName} was not found"
+                : $"{resourceName} with identifier '{identifier}' was not found";
+
+            return new Error("Resource.NotFound", message);
+        }
+
+        public static Error AlreadyExists(string resourceName, string identifier = "")
+        {
+            string message = string.IsNullOrEmpty(identifier)
+                ? $"{resourceName} already exists"
+                : $"{resourceName} with identifier '{identifier}' already exists";
+
+            return new Error("Resource.AlreadyExists", message);
+        }
+    }
+    
+    public static class Auth
+    {
+        public static Error PasswordError(string message = "Error occurred while trying to assign password to the user")
+            => new("Auth.InvalidCredentials", message);
+
+        public static Error AccountLocked(string message = "Account is locked")
+            => new("Auth.AccountLocked", message);
+    }
+    
+    public static class User
+    {
+        public static Error EmailAlreadyExists(string email = "")
+        {
+
+            string message = string.IsNullOrWhiteSpace(email)
+                ? "Email is taken"
+                : $"Email '{email}' is taken";
+            
+            return new Error("User.EmailAlreadyExists", message);
+        }
+
+        public static Error UsernameAlreadyExists(string username = "")
+        {
+            string message = string.IsNullOrWhiteSpace(username)
+                ? "Username is taken"
+                : $"Email is {username}";
+
+            return new Error("User.UsernameAlreadyExists", message);
+        }
+
+        public static Error NotFound(string identifier = "")
+        {
+            string message = string.IsNullOrWhiteSpace(identifier)
+                ? "User not found"
+                : $"User with identifier '{identifier}' is not found";
+
+            return new Error("User.NotFound", message);
+        }
+    }
+}
---
+++ WorkoutTrackerApi/Services/Results/ServiceResult.cs
@@ -0,0 +1,60 @@
+namespace WorkoutTrackerApi.Services.Results;
+
+public class ServiceResult
+{
+    public bool IsSucceeded { get; }
+
+    public IReadOnlyList<Error> Errors { get; }
+
+    protected ServiceResult(bool isSucceeded, IReadOnlyList<Error> errors)
+    {
+        IsSucceeded = isSucceeded;
+        Errors = errors;
+    }
+
+    public static ServiceResult Success() => new(true, []);
+
+    public static ServiceResult Failure(params Error[] errors)
+    {
+        if (errors.Length == 0)
+            throw new ArgumentException("At least one error must be provided within a failure");
+
+        return new ServiceResult(false, errors.AsReadOnly());
+
+    }
+
+}
+
+public class ServiceResult<T> : ServiceResult
+{
+    private T? Payload { get; }
+
+    private ServiceResult(bool isSucceeded, IReadOnlyList<Error> errors) : base(isSucceeded, errors)
+    {
+        Payload = default;
+    }
+    
+    private ServiceResult(bool isSucceeded, IReadOnlyList<Error> errors, T? payload) : base(isSucceeded, errors)
+    {
+        Payload = payload;
+    }
+
+    public new static ServiceResult<T> Success() => new(true, []);
+
+    public static ServiceResult<T> Success(T payload)
+    {
+        if (payload is null)
+            throw new ArgumentNullException(nameof(payload), "Payload cannot be null");
+
+        return new ServiceResult<T>(true, [], payload);
+    }
+
+    public new static ServiceResult<T> Failure(params Error[] errors)
+    {
+        if(errors.Length == 0)
+            throw new ArgumentException("At least one error must be provided within a failure");
+
+        return new ServiceResult<T>(false, errors.AsReadOnly());
+    }
+
+}
