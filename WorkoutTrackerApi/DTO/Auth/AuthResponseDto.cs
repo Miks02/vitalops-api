0d6f40c45882f22c7f518fe630d4c1d1b697312b
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-31 11:35:30 +0100

    feat(dto): Add RefreshToken to AuthResponseDto and make User property nullable

--- WorkoutTrackerApi/DTO/Auth/AuthResponseDto.cs
+++ WorkoutTrackerApi/DTO/Auth/AuthResponseDto.cs
@@ -5,5 +5,6 @@ namespace WorkoutTrackerApi.DTO.Auth;
 public class AuthResponseDto
 {
     public string AccessToken { get; set; } = null!;
-    public UserDto User { get; set; } = null!;
+    public string RefreshToken { get; set; } = null!;
+    public UserDto? User { get; set; } = null!;
 }
