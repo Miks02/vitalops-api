3a5e9204d3b821d79974bae4ec76521ca1adef4e
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-31 14:48:34 +0100

    feat(model): Extend ExerciseEntry with more properties

---
+++ WorkoutTrackerApi/Migrations/20251231134744_ExtendExerciseEntry.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkoutTrackerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendExerciseEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CardioType",
+                table: "ExerciseEntries",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IntervalsCount",
+                table: "ExerciseEntries",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxHeartRate",
+                table: "ExerciseEntries",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "PaceMinPerKm",
+                table: "ExerciseEntries",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RestIntervalSec",
+                table: "ExerciseEntries",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WorkIntervalSec",
+                table: "ExerciseEntries",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CardioType",
+                table: "ExerciseEntries");
+
+            migrationBuilder.DropColumn(
+                name: "IntervalsCount",
+                table: "ExerciseEntries");
+
+            migrationBuilder.DropColumn(
+                name: "MaxHeartRate",
+                table: "ExerciseEntries");
+
+            migrationBuilder.DropColumn(
+                name: "PaceMinPerKm",
+                table: "ExerciseEntries");
+
+            migrationBuilder.DropColumn(
+                name: "RestIntervalSec",
+                table: "ExerciseEntries");
+
+            migrationBuilder.DropColumn(
+                name: "WorkIntervalSec",
+                table: "ExerciseEntries");
+        }
+    }
+}
