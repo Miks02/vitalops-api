5deec0bc855ff87fc278ddf5f1dccf5d9ba71892
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-05 23:44:06 +0100

    feat(dto): Add WorkoutPageDto and WorkoutSummaryDto

---
+++ WorkoutTrackerApi/DTO/Workout/WorkoutPageDto.cs
@@ -0,0 +1,11 @@
+﻿using WorkoutTrackerApi.DTO.Global;
+
+namespace WorkoutTrackerApi.DTO.Workout
+{
+    public class WorkoutPageDto
+    {
+        public PagedResult<WorkoutListItemDto> PagedWorkouts { get; set; } = null!;
+
+        public WorkoutSummaryDto WorkoutSummary { get; set; } = null!;
+    }
+}
---
+++ WorkoutTrackerApi/DTO/Workout/WorkoutSummaryDto.cs
@@ -0,0 +1,13 @@
+using System;
+using WorkoutTrackerApi.Enums;
+
+namespace WorkoutTrackerApi.DTO.Workout;
+
+public class WorkoutSummaryDto
+{
+    public int ExerciseCount { get; set; }
+
+    public DateTime LastWorkoutDate { get; set; }
+
+    public ExerciseType FavoriteExerciseType { get; set; }
+}
