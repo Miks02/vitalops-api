f97aae343935db142877e45e995327c1a6fb966f
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-07 14:09:32 +0100

    refactor(dto): Remove PageSize from QueryParams and convert Sort to lower case in constructor

--- WorkoutTrackerApi/DTO/Global/QueryParams.cs
+++ WorkoutTrackerApi/DTO/Global/QueryParams.cs
@@ -3,17 +3,15 @@ namespace WorkoutTrackerApi.DTO.Global;
 public class QueryParams
 {
     public int Page { get; set; }
-    public int PageSize { get; set; } 
     public string Search { get; set; }
     public string Sort { get; set; }
     public DateTime? Date { get; set; }
 
-    public QueryParams(int page, int pageSize, string search, string sort, DateTime? date)
+    public QueryParams(int page, string search, string sort, DateTime? date)
     {
         Page = page;
-        PageSize = pageSize;
         Search = search;
-        Sort = sort;
+        Sort = sort.ToLower();
         Date = date;
     }
 }
