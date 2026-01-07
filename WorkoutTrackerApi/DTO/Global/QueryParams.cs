9bc1c4dd64ab36647b411b9cfe8b5714095c3238
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-07 13:27:38 +0100

    feat(dto): Add Date property to the QueryParams dto

--- WorkoutTrackerApi/DTO/Global/QueryParams.cs
+++ WorkoutTrackerApi/DTO/Global/QueryParams.cs
@@ -4,14 +4,16 @@ public class QueryParams
 {
     public int Page { get; set; }
     public int PageSize { get; set; } 
-    public string? Search { get; set; }
+    public string Search { get; set; }
     public string Sort { get; set; }
+    public DateTime? Date { get; set; }
 
-    public QueryParams(int page, int pageSize, string? search, string sort)
+    public QueryParams(int page, int pageSize, string search, string sort, DateTime? date)
     {
         Page = page;
         PageSize = pageSize;
         Search = search;
         Sort = sort;
+        Date = date;
     }
 }
