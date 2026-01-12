852333bc82eacc08e33620401a5320693cd57ea3
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-12 14:01:58 +0100

    feat(dto): Add ProfilePageDto and UserDetailsDto

---
+++ WorkoutTrackerApi/DTO/User/ProfilePageDto.cs
@@ -0,0 +1,14 @@
+﻿using WorkoutTrackerApi.DTO.Workout;
+
+namespace WorkoutTrackerApi.DTO.User
+{
+    public class ProfilePageDto
+    {
+        public UserDetailsDto UserDetails { get; set; } = null!;
+        public IReadOnlyList<WorkoutListItemDto> RecentWorkouts { get; set; } = [];
+
+        public int? WorkoutStreak { get; set; }
+        public int? DailyCalorieGoal { get; set; }
+
+    }
+}
---
+++ WorkoutTrackerApi/DTO/User/UserDetailsDto.cs
@@ -0,0 +1,26 @@
+﻿using WorkoutTrackerApi.Enums;
+
+namespace WorkoutTrackerApi.DTO.User
+{
+    public class UserDetailsDto
+    {
+        public string FullName { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? ImagePath { get; set; }
+
+        public Gender? Gender { get; set; }
+        public double? Weight { get; set; }
+        public double? Height { get; set; }
+
+        public AccountStatus AccountStatus { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        public DateTime RegisteredAt { get; set; }
+
+        public int? Age => DateOfBirth.HasValue
+            ? DateTime.UtcNow.Year - DateOfBirth.Value.Year - (DateTime.UtcNow.DayOfYear < DateOfBirth.Value.DayOfYear ? 1 : 0)
+            : null;
+
+    }
+}
