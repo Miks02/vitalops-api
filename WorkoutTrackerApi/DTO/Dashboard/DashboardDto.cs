985ef13b9c744ac2765c694bf3ea96c7bac6f021
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-17 23:43:10 +0100

    feat(dto): Add DashboardDto

---
+++ WorkoutTrackerApi/DTO/Dashboard/DashboardDto.cs
@@ -0,0 +1,14 @@
+﻿using WorkoutTrackerApi.DTO.Workout;
+
+namespace WorkoutTrackerApi.DTO.Dashboard
+{
+    public class DashboardDto
+    {
+        public double DailyCalories { get; set; }
+        public double WaterIntake { get; set; }
+        public double AverageSleep { get; set; }
+        public DateTime? LastWorkoutDate { get; set; }
+        public IReadOnlyList<WorkoutListItemDto> RecentWorkouts { get; set; } = new List<WorkoutListItemDto>();
+
+    }
+}
