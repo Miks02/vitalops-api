56d16a347c655921f303e2ee6d6cb57c8c303f29
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-17 23:46:33 +0100

    feat(controller): Add a DashboardController with GetDashboard endpoint

---
+++ WorkoutTrackerApi/Controllers/DashboardController.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WorkoutTrackerApi.DTO.Dashboard;
+using WorkoutTrackerApi.Services.Interfaces;
+
+namespace WorkoutTrackerApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly IDashboardService _dashboardService;
+        public DashboardController(IDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<DashboardDto>> GetDashboard(CancellationToken cancellationToken = default)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            return await _dashboardService.LoadDashboardAsync(userId!, cancellationToken);
+
+        }
+    }
+}
