698508465e03e04b49e9b23411b0f10d4ebdab0b
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-20 03:02:34 +0100

    feat(helper): Add a Utility helper class

---
+++ VitalOps.API/Helpers/Utility.cs
@@ -0,0 +1,16 @@
+﻿namespace VitalOps.API.Helpers
+{
+    public static class Utility
+    {
+        public static TimeSpan? ValidateMinutesAndSeconds(int? minutes, int? seconds)
+        {
+            if (minutes is null || seconds is null)
+                return null;
+
+            TimeSpan fromMinutes = TimeSpan.FromMinutes((double)minutes);
+            TimeSpan fromSeconds = TimeSpan.FromSeconds((double)seconds);
+
+            return fromMinutes + fromSeconds;
+        }
+    }
+}
